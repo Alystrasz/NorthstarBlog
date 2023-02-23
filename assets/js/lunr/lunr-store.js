1e46d502960a22602e3fe2cab4bf5778d0c7c4f0
GeckoEidechse <GeckoEidechse@users.noreply.github.com>
2023-02-23 12:35:56 +0000

    jekyll build from Action bedf5f42bb04a371adc094c29cd2aaaa7dd830bb

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Frontier News Network: One year already!",
+        "excerpt":"What? One year… already?! Over 365 days ago, Bob did the first public release of Northstar, enabling players to host their own Titanfall2 servers with custom rules sets and gamemodes! We’ve come a long way since then: 235072 total unique players; 45 game releases; 76368 Discord community members. Even if...","categories": [],
+        "tags": ["FNN"],
+        "url": "/blog/one-year/",
+        "teaser": null
+      },{
+        "title": "Titanfall2 Security: Unrestricted server command execution via Squirrel Script",
+        "excerpt":"A few weeks ago, we accidentally discovered that Squirrel server scripts were made accessible on public Titanfall2 servers. Here’s what we found, how we reported it, and how it got resolved. The discovered exploit allows an adversary to run any Squirrel script on the server within the bounds of Titanfall2’s...","categories": [],
+        "tags": ["Security"],
+        "url": "/blog/vanilla-unrestricted-server-script/",
+        "teaser": null
+      }]
